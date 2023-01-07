dbd601ee895d3e9404e21c596aa83566dbbe72c1
KOSASIH <kosasihg88@gmail.com>
2023-01-07 12:49:05 +0700

    Update exotic_app.py

--- exotic_app.py
+++ exotic_app.py
@@ -7,8 +7,8 @@ import streamlit as st
 """
 # Welcome to Exotic!
 Edit `/exotic_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+If you have any questions, checkout our [documentation](https://github.com/KOSASIH/Exotic/tree/main/docs) and [community
+forums](https://github.com/KOSASIH/Exotic/discussions).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
