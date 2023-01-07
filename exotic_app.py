12d4fedbc1ae8a840481f1c545fd62cd7ce9dd97
KOSASIH <kosasihg88@gmail.com>
2023-01-07 12:43:55 +0700

    Update exotic_app.py

--- exotic_app.py
+++ exotic_app.py
@@ -7,6 +7,8 @@ import streamlit as st
 """
 # Welcome to Exotic!
 Edit `/exotic_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
@@ -31,4 +33,3 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-Footer
